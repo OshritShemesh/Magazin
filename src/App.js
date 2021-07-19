ebee4c811480d1afb672f60d13d2494385355bb2
odelRashid <odelrash.com>
2021-07-19 11:33:38 +0300

    base_curd

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import MainRouter from './mainrouter';
 import { Provider } from 'react-redux';
 import store from './redux/store/store'
 // import UploadImage from './comps/uplaodimage'
+import ViewMagazins from './comps/ViewMagazins'
+
 function App() {
   return (
     <div>
-    
+      
       <Provider store={store}>
         <MainRouter></MainRouter>
+        <ViewMagazins></ViewMagazins>
       </Provider>
 
       {/* <ChoosenPic></ChoosenPic> */}
---
+++ src/redux/middleware/crud.magazin.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+
+export const getAllMagazin = ({ dispatch, getState }) => next => action => {
+
+    if (action.type == 'GET_ALL_MAGAZIN') {
+
+
+        
+    }
+
+
+    return next(action);
+} 
