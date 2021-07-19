9d7b313f0cbf5f1f844f7c91be599e87a251d291
OshritShemesh <oshrit.shemesh99@gmail.com>
2021-07-19 12:39:03 +0300

    create magazine func & component

--- src/comps/createMagazine.js
+++ src/comps/createMagazine.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { action } from '../redux/action';
 
-export default function CreateMagazine() {
+function CreateMagazine(props) {
 
     const [magazine, setMagazine] = useState({
         title: "",
@@ -10,10 +12,12 @@ export default function CreateMagazine() {
 
     function addMagazine() {
         console.log(magazine);
+        props.createMagazine(magazine);
     }
 
     return (
         <>
+            {props.user && props.user.firstName}
             <input name='title' type="text" onChange={e => setMagazine({ ...magazine, title: e.target.value })}></input>
             <textarea name="subTitle" cols="50" rows="3" onChange={e => setMagazine({ ...magazine, subTitle: e.target.value })}></textarea>
             <textarea name="content" cols="50" rows="10" onChange={e => setMagazine({ ...magazine, content: e.target.value })}></textarea>
@@ -21,3 +25,19 @@ export default function CreateMagazine() {
         </>
     )
 }
+
+
+export default connect(
+    (state) => {
+        return {
+            // user: state.user.user
+        }
+    },
+    (dispatch) => {
+        return {
+            createMagazine: (magazin) => {
+                dispatch(action.createMagazine(magazin))
+            }
+        }
+    }
+)(CreateMagazine)
